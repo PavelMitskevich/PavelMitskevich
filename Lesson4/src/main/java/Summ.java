f57507926239dde48d09d091ac6b6f8d7b886853
PavelMitskevich <paulmitskevich@gmail.com>
2021-10-17 22:21:33 +0300

    урок 4 д/з

--- Lesson4/src/main/java/Summ.java
+++ Lesson4/src/main/java/Summ.java
@@ -1,12 +1,24 @@
 public class Summ {
     public static void main(String[] args) {
-        summ(4, 8);
+        summ(-4, -8);
+        summ(-4, 8);
+        summ(4, -8);
+        summ(0, -8);
+        summ(4, 0);
     }
 
     public static int summ(int a, int b) {
         int summ = 0;
-        for (int i = 0; i < b; i++) {
-            summ += a;
+        if (b < 0) {
+            for (int i = 0; i > b; i--) {
+                summ -= a;
+            }
+        } else if (b > 0) {
+            for (int i = 0; i < b; i++) {
+                summ += a;
+            }
+        } else {
+            summ = 0;
         }
         System.out.println(summ);
         return summ;
