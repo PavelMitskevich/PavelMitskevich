7b5da66e0210f10c82aba6e617abcbef97ea4a36
PavelMitskevich <paulmitskevich@gmail.com>
2021-11-14 21:50:02 +0300

    урок 11/1 д/з

---
+++ Lesson11/src/main/java/first/Main.java
@@ -0,0 +1,9 @@
+package first;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Audi", 240.3, 15000);
+
+        car.start();
+    }
+}
---
+++ Lesson11/src/main/java/first/WrongStartException.java
@@ -0,0 +1,10 @@
+package first;
+
+public class WrongStartException extends Exception {
+    public WrongStartException(String message) {
+        super(message);
+    }
+
+    public WrongStartException() {
+    }
+}
