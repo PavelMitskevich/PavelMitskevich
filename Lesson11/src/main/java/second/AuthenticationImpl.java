8bfa6975add5aeba5b6516e7156f516f7a29fc3f
PavelMitskevich <paulmitskevich@gmail.com>
2021-11-16 22:26:46 +0300

    урок 11/2 д/з

--- Lesson11/src/main/java/second/AuthenticationImpl.java
+++ Lesson11/src/main/java/second/AuthenticationImpl.java
@@ -9,10 +9,10 @@ public class AuthenticationImpl implements AuthenticationService {
 
     @Override
     public boolean checkAuthorization(String login, String password, String confirmPassword) throws Exception {
-        if (!login.matches("\\w") && login.length() > 19) {
+        if (!login.matches("\\w+") || login.length() > 19) {
             throw new WrongLoginException("В поле ЛОГИН введены некорректные данные");
         }
-        if (!Pattern.matches("\\w", password) && password.length() > 19) {
+        if (!Pattern.matches("\\w+", password) || password.length() > 19) {
             throw new WrongPasswordException("В поле ПАРОЛЬ введены некорректные данные");
         }
         if (!confirmPassword.equals(password)) {
